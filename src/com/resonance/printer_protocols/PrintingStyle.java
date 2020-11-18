6bd3192c8937e451f401f9766674f82b2b0570cd
DarthCat1 <1stLawgiver@gmail.com>
2020-11-18 11:14:39 +0200

    Added method`s setUnderline, setLineSpacing, setDefaultLineSpacing, setReverseColor, setCharSize. Added enum`s UnderlineMode and CharSize.

--- src/com/resonance/printer_protocols/PrintingStyle.java
+++ src/com/resonance/printer_protocols/PrintingStyle.java
@@ -9,15 +9,37 @@ public interface PrintingStyle {
     byte [] setBold(boolean bold);
     byte [] setCharFonts(CharFonts charFonts);
     byte [] setAlignment(Alignment alignment);
+    byte [] setUnderline(UnderlineMode underlineMode);
+    byte [] setLineSpacing(byte lineSpacing);
+    byte [] setDefaultLineSpacing();
+    byte [] setReverseColor(boolean reverseColor);
+    byte [] setCharSize(CharSize width, CharSize height);
 
     enum Alignment {
         LEFT,
         CENTER,
-        RIGHT;
+        RIGHT
     }
 
     enum CharFonts {
         FONT_A,
-        FONT_B;
+        FONT_B
+    }
+
+    enum UnderlineMode {
+        UNDERLINE_OFF(),
+        UNDERLINE_1_DOT(),
+        UNDERLINE_2_DOT()
+    }
+
+    enum CharSize {
+        x1_NORMAL(),
+        x2_DOUBLE(),
+        x3(),
+        x4(),
+        x5(),
+        x6(),
+        x7(),
+        x8()
     }
 }
