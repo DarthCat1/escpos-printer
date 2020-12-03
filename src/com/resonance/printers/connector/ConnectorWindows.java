19709f18c71c8626a25097ff42488bb3bd6e8da1
DarthCat1 <1stLawgiver@gmail.com>
2020-12-03 10:58:27 +0200

    Added static method that returns a PrintServices list. Added setter and constructor with parameter String printServiceName.

--- src/com/resonance/printers/connector/ConnectorWindows.java
+++ src/com/resonance/printers/connector/ConnectorWindows.java
@@ -1,6 +1,7 @@
 package com.resonance.printers.connector;
 
 import javax.print.*;
+import java.awt.print.PrinterJob;
 
 public class ConnectorWindows implements Connector {
 
@@ -10,12 +11,43 @@ public class ConnectorWindows implements Connector {
         this.printService = printService;
     }
 
+    public ConnectorWindows (String printServiceName) throws IllegalPrintServiceNameException {
+        setPathway(printServiceName);
+    }
+
     public void setPrintService(PrintService printService) {
         this.printService = printService;
     }
 
     @Override
-    public void setPathway(String pathway) {
+    public void setPathway(String printServiceName) throws IllegalPrintServiceNameException {
+        PrintService[] printServices = PrinterJob.lookupPrintServices();
+        PrintService foundService = null;
+
+        for (PrintService service : printServices) {
+            if (service.getName().equals(printServiceName)) {
+                foundService = service;
+                break;
+            }
+        }
+        if (foundService != null) {
+            printService = foundService;
+            return;
+        }
+
+        for (PrintService service : printServices) {
+            if (service.getName().compareToIgnoreCase(printServiceName) == 0) {
+                foundService = service;
+                break;
+            }
+        }
+        if (foundService != null) {
+            printService = foundService;
+        }
+
+        else {
+            throw new IllegalPrintServiceNameException("printServiceName is not found");
+        }
     }
 
     @Override
@@ -34,4 +66,14 @@ public class ConnectorWindows implements Connector {
         DocPrintJob job = printService.createPrintJob();
         job.print(d, null);
     }
+
+    public static String[] getPrintServicesNames() {
+        PrintService[] printServices = PrinterJob.lookupPrintServices();
+        String[] printServicesNames = new String[printServices.length];
+        for (int i = 0; i < printServices.length; ++i) {
+            printServicesNames[i] = printServices[i].getName();
+        }
+        return printServicesNames;
+    }
+
 }
